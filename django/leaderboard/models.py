985a3219c8a8bd523da7af65f56a1f96d969787f
Arpan-amiqo <arpan@amiqo.life>
2025-10-27 21:14:20 -0400

    Get and Post (#9)
    * set get and post
    
    * secret keys
    
    ---------
    
    Co-authored-by: Redrexy <arpansubba2002@gmail.com>

--- django/leaderboard/models.py
+++ django/leaderboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Leaderboard(models.Model):
+    name = models.CharField(max_length=20)
+    score = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name} - {self.score}"
